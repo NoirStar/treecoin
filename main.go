5fa8612968ca42bedba5d594fb5523732551d5eb
namwoo.kim <sky_9233@naver.com>
2021-08-13 00:38:39 +0900

    tc

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	chain := blockchain.GetBlockchain()
 	chain.AddBlock("Second Block")
 	chain.AddBlock("Third Block")
+	chain.AddBlock("Last Block")
 	for _, block := range chain.AllBlocks() {
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %s\n", block.Hash)
